f8ec3356bc2dd970f547837a3d23ff2e28c0e75b
Antoinegtir <antoine.gonthier@epitech.eu>
2024-09-03 17:40:15 +0200

    👉 add bottom navbar if the height value is higher than the width

--- client/lib/pages/home.dart
+++ client/lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:soul_connection/pages/constants/constants.dart';
 import 'package:soul_connection/pages/models/user.module.dart';
+import 'package:soul_connection/pages/widget/drawerbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.user});
@@ -11,54 +12,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int tabIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      bottomNavigationBar: dw(context) <= 700
+          ? BottomNavigationBar(
+              onTap: (int index) {
+                setState(() {
+                  tabIndex = index;
+                });
+              },
+              selectedItemColor: Colors.blue,
+              currentIndex: tabIndex,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home, color: Colors.blue),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.people, color: Colors.blue),
+                  label: 'Coaches',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person, color: Colors.blue),
+                  label: 'Customers',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.bar_chart, color: Colors.blue),
+                  label: 'Statistics',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.lightbulb, color: Colors.blue),
+                  label: 'Tips',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.event, color: Colors.blue),
+                  label: 'Events',
+                ),
+              ],
+            )
+          : null,
       body: SizedBox(
         height: dh(context),
         width: dw(context),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Row(
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(200),
-              child: Image.memory(
-                widget.user.profilePic!,
-                height: 180,
+            if (dw(context) > 700)
+              SizedBox(
+                width: dw(context) * 0.2,
+                height: dh(context),
+                child: Drawerbar(
+                  user: widget.user,
+                  onTabChange: (int tab) {
+                    setState(() {
+                      tabIndex = tab;
+                    });
+                  },
+                ),
               ),
+            Container(
+              color: Colors.grey,
+              width: 1,
+              height: dh(context) - 80,
             ),
-            Text(
-              widget.user.surname! + widget.user.name!,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 25,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Text(
-              widget.user.birthDate!,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 25,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Text(
-              widget.user.gender!,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 25,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Text(
-              widget.user.work!,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 25,
-                fontWeight: FontWeight.w500,
-              ),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              width: (dw(context) * 0.8) - 1,
+              height: dh(context),
+              color: [Colors.white, Colors.red][tabIndex % 2],
             ),
           ],
         ),
