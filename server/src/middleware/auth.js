949326cca1a13d9e245ebfc749ee50d4b8e8f287
ciremiadrk <aimeric.rouyer@gmail.com>
2024-09-04 11:10:20 +0200

    :sparkles: finished implementing basic function independant from the given api ( login register ...)

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,56 +1,57 @@
-const User = require('../database/models/users.js')
+const UserModel = require('../database/models/users.js')
+const SessionModel = require('../database/models/session.js')
 const jwt = require('jsonwebtoken');
 const sendApiData = require("./api-formatter.js");
 
-async function get_user_from_session(req, res, next, bloquant = true) {
+async function checkAuthenticated(req, res, next) {
     try {
-        const session = req.headers.session;
-        var decoded = null;
+        const GivenSession = req.headers.session;
+        var decodedSession = null;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
         req.user = null;
-        if (!session || session == null || session == undefined || session == "null")
-            return sendApiData(req, res, 401, "noSession", "You don't appaears to have a valid session", null, null, null, null);
+        if (!GivenSession) return sendApiData(req, res, 401, "noSession", "You didn't provide any session", null, null, null, null);
 
         try {
-            decoded = jwt.verify(session, process.env.SECRET);
+            decodedSession = await jwt.verify(GivenSession, process.env.SECRET);
         } catch (err) {
-            return sendApiData(req, res, 401, "invalidSession", "It appears that your session is invalid", null, null, null, null);
+            decodedSession = null;
         }
+        if (!decodedSession) return sendApiData(req, res, 401, "invalidSession", "It appears that your session is invalid", null, null, null, null);
 
-        if (!decoded) return sendApiData(req, res, 401, "invalidSession", "It appears that your session is invalid", null, null, null, null);
+        await SessionModel.findOne({
+            unique_session_id: decodedSession.session_id
+        }).then(async function (FoundSession) {
+            if (!FoundSession) return invalid_session(req, res);
 
-        Session.findOne({ unique_session_id: decoded.session_id }).then(function (session) {
-            if (!check_non_null_session(session))
-                return sendApiData(req, res, 401, "invalidSession", "It appears that your session is invalid or expired", null, null, null, null);
+            await UserModel.findOne({
+                link_session_id: FoundSession.signed_id
+            }).then(async function (CorrespondingUser) {
 
-            User.findOne({ link_session_id: session.signed_id }).then(function (user) {
-                if (!verif_session_data(session, user, ip))
-                    return sendApiData(req, res, 401, "invalidSession", "It appears that your session is invalid", null, null, null, null);
-                req.user = user;
+                await verif_session_data(FoundSession, CorrespondingUser, ip)
+
+                req.user = CorrespondingUser;
+                req.session = FoundSession;
                 return next();
-            }).catch(function (err) {
-                return sendApiData(req, res, 500, "errorOccured", "An error occured while trying to verify your session", null, err, null, null);
-
-            });
-        }).catch(function (err) {
-            return sendApiData(req, res, 500, "errorOccured", "An error occured while trying to verify your session", null, err, null, null);
-        });
+            })
+        })
     } catch (err) {
+        console.log(err);
         return sendApiData(req, res, 500, "errorOccured", "An error occured while trying to verify your session", null, err, null, null);
     }
 }
 
 
-async function check_non_null_session(session) {
-    return (session && session != null && session != undefined && session != "null" && session.expire > Date.now());
-}
-
 async function verif_session_data(session, user, ip) {
-    return (session.expire < Date.now() || session.expire == null || session.expire == undefined ||
-        session.signed_id == null || session.signed_id == undefined ||
-        session.signed_id != user.link_session_id || session.connexionIp != ip ||
+    if (session.expire == null || session.expire < Date.now() ||
+        session.signed_id == null || !user.link_session_id.includes(session.signed_id) ||
+        session.connexionIp == null || session.connexionIp != ip ||
         session.user_signed_id != user.unique_id)
+        return invalid_session(req, res);
 }
 
-module.exports = get_user_from_session;
+function invalid_session(req, res) {
+    return sendApiData(req, res, 401, "invalidSession", "It appears that your session is invalid", null, null, null, null);
+}
+
+module.exports = checkAuthenticated;
--- server/src/router/api/api.query.js
+++ server/src/router/api/api.query.js
@@ -3,6 +3,6 @@ const get_user_from_session = require("../../middleware/auth.js");
 
 module.exports = function (app) {
     app.get("/user_profile_info_api", get_user_from_session, api_interactions.user_profile_info_api);
-    app.get("/navbar_info_api", (req, res, next) => get_user_from_session(req, res, next, false), api_interactions.navbar_info_api);
+    app.get("/navbar_info_api", get_user_from_session, api_interactions.navbar_info_api);
 
 };
