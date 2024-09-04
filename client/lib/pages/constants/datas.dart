20c1cf5e49cbf1009eafdf96eda318a8d05ff223
Antoinegtir <antoine.gonthier@epitech.eu>
2024-09-04 12:40:48 +0200

    📖 export raw data in a constant file

---
+++ client/lib/pages/constants/datas.dart
@@ -0,0 +1,273 @@
+List<String> tableCoaches = [
+  '#',
+  'Name',
+  'Birth date',
+  'Customers',
+  'Last connection'
+];
+
+List<Map<String, dynamic>> users = [
+  {
+    'name': 'Jean Patrick',
+    'birth date': '09-01-1987',
+    'customers': 'Edit list',
+    'last connection': '18-07-2024',
+  },
+  {
+    'name': 'Patrick Michel',
+    'birth date': '29-01-1943',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '12-01-1921',
+    'customers': 'Edit list',
+    'last connection': '15-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+  {
+    'name': 'Jean Patrick',
+    'birth date': '14-01-1999',
+    'customers': 'Edit list',
+    'last connection': '12-07-2024',
+  },
+];
+
+List<String> clients = [
+  'Louis Bagneul',
+  'Véronique Tanson',
+  'Angele Labelge',
+  'Nicolas Tarabavich'
+];
+
+List<Map<String, dynamic>> customers = [
+  {
+    'name': 'Louis Delanata',
+    'id': 1,
+    'birthday': '02/03/2006',
+    'address': '3 Rue de al Tour 34000 Montpelier, France'
+  },
+  {
+    'name': 'Romain Ruiz',
+    'id': 2,
+    'birthday': '09/06/2016',
+    'address': '78 Rue de la mélinière 44200 Nantes, France'
+  },
+  {
+    'name': 'Paul Boulanger',
+    'id': 3,
+    'birthday': '12/03/1999',
+    'address': '26 Rue des bougainvilliers 97400 Saint-Denis, France'
+  },
+  {
+    'name': 'Max Delanata',
+    'id': 3,
+    'birthday': '09/06/1996',
+    'address': '34 Rue des pivoines 97400 Saint-Denis, France'
+  },
+];
+
+List<String> table1Labels = ['Date', 'Amount', 'Comment'];
+List<String> table2Labels = ['Date', 'Rating', 'Report', 'Source'];
+
+List<Map<String, dynamic>> contentTable1 = [
+  {
+    'date': '09/06/1996',
+    'amount': '100€',
+    'comment': 'Subscription',
+  },
+  {
+    'date': '09/06/1996',
+    'amount': '100€',
+    'comment': 'Subscription',
+  },
+  {
+    'date': '09/06/1996',
+    'amount': '100€',
+    'comment': 'Subscription',
+  },
+  {
+    'date': '09/06/1996',
+    'amount': '100€',
+    'comment': 'Subscription',
+  },
+];
+
+List<Map<String, dynamic>> contentTable2 = [
+  {
+    'date': '09/06/1996',
+    'rating': '3/5',
+    'report': 'good moment',
+    'source': 'Google',
+  },
+  {
+    'date': '09/06/1996',
+    'rating': '5/5',
+    'report': 'Dating app',
+    'source': 'Google',
+  },
+  {
+    'date': '09/06/1996',
+    'rating': '2/5',
+    'report': 'cool',
+    'source': 'All the bar',
+  },
+  {
+    'date': '09/06/1996',
+    'rating': '4/5',
+    'report': 'nice',
+    'source': 'yahoo',
+  },
+];
+
+List<String> tableLabels = ['seq', 'Product', 'Category', 'Period', 'Sales'];
+
+List<Map<String, dynamic>> products = [
+  {
+    'product': 'Tea ☕️',
+    'category': 'cat1',
+    'period': 'Jan-11',
+    'sales': '20K',
+  },
+  {
+    'product': 'Coffee ☕️',
+    'category': 'cat2',
+    'period': 'Jan-11',
+    'sales': '15K'
+  },
+  {
+    'product': 'Milk 🥛',
+    'category': 'cat3',
+    'period': 'Jan-11',
+    'sales': '5K',
+  },
+  {
+    'product': 'Cereal 🌾',
+    'category': 'cat4',
+    'period': 'Jan-11',
+    'sales': '30K'
+  },
+  {
+    'product': 'Chocolate 🍫',
+    'category': 'cat5',
+    'period': 'Jan-11',
+    'sales': '25K'
+  },
+  {
+    'product': 'Pasta 🍝',
+    'category': 'cat6',
+    'period': 'Jan-11',
+    'sales': '205K'
+  },
+  {
+    'product': 'Pizza 🍕',
+    'category': 'cat7',
+    'period': 'Jan-11',
+    'sales': '15K'
+  },
+  {
+    'product': 'Avocado 🥑',
+    'category': 'cat8',
+    'period': 'Jan-11',
+    'sales': '21K'
+  },
+  {
+    'product': 'Tomato 🍅',
+    'category': 'ca9',
+    'period': 'Jan-11',
+    'sales': '15K'
+  },
+];
+
+List<Map<String, dynamic>> data = [
+  {
+    'label': 'Chocolate',
+    'data': [200.0, 80.0, 10.0, 20.0, 25.0, 320.0],
+  },
+  {
+    'label': 'Milk',
+    'data': [200.0, 190.0, 180.0, 200.0, 250.0, 300.0],
+  },
+  {
+    'label': 'Cereal',
+    'data': [20.0, 190.0, 10.0, 20.0, 25.0, 30.0],
+  },
+];
