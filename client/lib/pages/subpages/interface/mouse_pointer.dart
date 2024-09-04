2557c440e4b1b31090c724ad6ce8c7b8eedb1466
Antoinegtir <antoine.gonthier@epitech.eu>
2024-09-04 18:11:45 +0200

    🗑️ remove useless hover function for mouse pointer abstract

--- client/lib/pages/subpages/interface/mouse_pointer.dart
+++ client/lib/pages/subpages/interface/mouse_pointer.dart
@@ -2,39 +2,8 @@ import 'package:flutter/widgets.dart';
 
 mixin HoverMixin<T extends StatefulWidget> on State<T> {
   int? hoveredIndex;
-  int? hoveredIndexProfile;
   double rotationX = 0;
   double rotationY = 0;
-
-  void onHover(PointerEvent event, BuildContext context) {
-    final RenderBox renderBox = context.findRenderObject() as RenderBox;
-    final size = renderBox.size;
-    final position = event.localPosition;
-
-    final middleX = size.width / 2;
-    final middleY = size.height / 2;
-
-    setState(() {
-      hoveredIndexProfile = 1;
-      rotationY = (((position.dx - middleX) / middleX) + 0.1) * 0.2;
-      rotationX = (((position.dy - middleY) / middleY) + 0.8) * 0.8;
-    });
-  }
-
-  void onHoverGraph(PointerEvent event, int index, BuildContext context) {
-    final RenderBox renderBox = context.findRenderObject() as RenderBox;
-    final size = renderBox.size;
-    final position = event.localPosition;
-
-    final middleX = size.width / 2;
-    final middleY = size.height / 2;
-
-    setState(() {
-      hoveredIndex = index;
-      rotationY = (((position.dx - middleX) / middleX) + 0.1) * 0.2;
-      rotationX = (((position.dy - middleY) / middleY) + 0.8) * 0.8;
-    });
-  }
 
   void onHoverCard(PointerEvent event, int index, RenderBox renderBox) {
     final size = renderBox.size;
@@ -52,7 +21,6 @@ mixin HoverMixin<T extends StatefulWidget> on State<T> {
 
   void onExit(PointerEvent event) {
     setState(() {
-      hoveredIndexProfile = null;
       hoveredIndex = null;
       rotationX = 0;
       rotationY = 0;
@@ -61,7 +29,7 @@ mixin HoverMixin<T extends StatefulWidget> on State<T> {
 
   Matrix4 getTransformMatrix() {
     return (Matrix4.identity()
-      ..setEntry(2, 2, 0.001)
+      ..setEntry(3, 2, 0.001)
       ..rotateX(rotationX)
       ..rotateY(rotationY));
   }
