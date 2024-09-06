dd096e940be1161aef9714eae43fed12b1e141de
ubuntu_server_ciremia <aimeric.rouyer@gmail.com>
2024-09-06 11:17:32 +0200

    :children_crossing: add some progress bar + multithread for the static scrapper

--- script/staticScrapper.py
+++ script/staticScrapper.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 import json
+import threading
 
 load_dotenv("./.env")
 bearrer_token = ""
@@ -17,6 +18,7 @@ email = os.getenv("MAIL")
 password = os.getenv("PASSWORD")
 
 bar_length = 60
+error_file_name = "errors.txt"
 
 url_enabled = {
     "employees": False,
@@ -46,7 +48,8 @@ else:
     request_headers["Authorization"] = "Bearer " + login_response.json()["access_token"]
     print("Logged in successfully")
 
-errors = []    
+errors = []
+progress_bars = {}
 
     
 def treat_errors(e, url):
@@ -58,34 +61,45 @@ def print_errors_summary():
         print("No errors occurred !")
         return
     print("Errors:")
-    for error in errors:
-        print("an error occurred in " + error["url"] + " with the following message: " + str(error["error"]))
+    with open(error_file_name, "a") as error_file:
+        for error in errors:
+            error_message = "an error occurred in " + error["url"] + " with the following message: " + str(error["error"])
+            #print(error_message)
+            error_file.write(error_message + "\n")
         
-def progress_bar(current, total, name):
-    if not total or not current:
-        name = "error"
-        current = 0
-        total = 1
-    middle_bar_length = bar_length - 6
-    name_length = len(name) + 2
+def update_progress_bar(current, total, name, id = None):
+    clear_screen()
+    progress_bars[name] = {"current": current, "total": total, "id": id}
+    for key_bar, bar in progress_bars.items():
+        if round(int(bar["total"]) / int(bar["current"])) == 1:
+            continue
+        if not bar["total"] or not bar["current"]:
+            key_bar = "error"
+            bar["current"] = 0
+            bar["total"] = 1
+        if bar["id"]:
+            key_bar = key_bar + " #" + str(bar["id"])
+        middle_bar_length = bar_length - 6
+        name_length = len(key_bar) + 2
 
-    half_top_bar = "=" * round((int(bar_length) - name_length) / 2)
-    top_bar = half_top_bar + " " + name + " " + half_top_bar
-    bottom_bar = "=" * bar_length
+        half_top_bar = "=" * round((int(bar_length) - name_length) / 2)
+        top_bar = half_top_bar + " " + key_bar + " " + half_top_bar
+        bottom_bar = "=" * bar_length
 
-    progress = int(current) / int(total)
-    
-    block = int(middle_bar_length * progress)
-    bar = "|| " + "=" * block + "-" * (middle_bar_length - block) + " ||"
+        progress = int(bar["current"]) / int(bar["total"])
+        
+        block = int(middle_bar_length * progress)
+        bar = "|| " + "=" * block + "-" * (middle_bar_length - block) + " ||"
 
-    print(top_bar)
-    print(bar)
-    print(bottom_bar)
+        print(top_bar)
+        print(bar)
+        print(bottom_bar)
     
 def clear_progress_bar():
-    print("\033[A" + " " * bar_length + "\033[A")
-    print("\033[A" + " " * bar_length + "\033[A")
-    print("\033[A" + " " * bar_length + "\033[A")
+    number_of_spaces = bar_length + 1
+    number_of_lines = 4
+    for i in range(number_of_lines):
+        print("\033[A" + " " * number_of_spaces + "\033[A")
     
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -95,11 +109,12 @@ def make_request(url, is_image=False):
     try:
         response = requests.request("get", url, headers=request_headers)
         if response.status_code != 200:
-            print("Error: " + str(response.status_code))
             if not is_image:
                 print(response.json())
+                treat_errors("Error fetching : " + url + ", Got status code :" + str(response.status_code) + ", Got response message :" + json.dumps(response.json()), url)
             else:
                 print(response)
+                treat_errors("Error fetching : " + url + ", Got status code :" + str(response.status_code), url)
             return None
         if not is_image:
             return response.json()
@@ -118,31 +133,32 @@ def fetch_employee():
         api_employee = make_request(base_url + "/api/employees")
         for employee in api_employee:
             try:
-                progress_bar(str(api_employee.index(employee)), str(len(api_employee)), "employee")
+                update_progress_bar(str(api_employee.index(employee)), str(len(api_employee)), "employee")
                 
                 # get employees small data
-                if not db["smallemployee"].find_one(employee):
-                    db["smallemployee"].insert_one(employee)
+                employee = {**employee, "small_employee_id": str(employee["id"]), "employee_id": str(employee["id"])}
+                if not db["small_employee"].find_one(employee):
+                    db["small_employee"].insert_one(employee)
 
                 # get employees full data
                 full_employee = make_request(base_url + "/api/employees/" + str(employee["id"]))
+                full_employee = {**full_employee, "employee_id": str(employee["id"])}
                 if not db["employee"].find_one(full_employee):
                     db["employee"].insert_one(full_employee)
                 
                 # get employees image
                 employee_image = make_request(base_url + "/api/employees/" + str(employee["id"]) + "/image", True)
-                query = {"employee_id": str(employee["id"]), "image": employee_image}
+                query = {"employee_id": str(employee["id"]), "employee_image_id": str(employee["id"]), "image": employee_image}
                 if not db["employee_image"].find_one(query):
                     db["employee_image"].insert_one(query)
 
-                clear_progress_bar()
             except Exception as e:
                 treat_errors(e, base_url + "/api/employees/" + str(employee["id"]))
                 continue
     except Exception as e:
         treat_errors(e, base_url + "/api/employees")
         pass
-    clear_screen()
+    
 
 def fetch_customers():
     # --------------------- CUSTOMER ---------------------
@@ -151,20 +167,22 @@ def fetch_customers():
         api_customer = make_request(base_url + "/api/customers")
         for customer in api_customer:
             try:
-                progress_bar(str(api_customer.index(customer)), str(len(api_customer)), "customer #" + str(customer["id"]))
+                update_progress_bar(str(api_customer.index(customer)), str(len(api_customer)), "customer", customer["id"])
 
                 # get costumers small data
-                if not db["smallcustomer"].find_one(customer):
-                    db["smallcustomer"].insert_one(customer)
+                customer = {**customer, "small_customer_id": str(customer["id"]), "customer_id": str(customer["id"])}
+                if not db["small_customer"].find_one(customer):
+                    db["small_customer"].insert_one(customer)
 
                 # get costumers full data
                 full_customer = make_request(base_url + "/api/customers/" + str(customer["id"]))
+                full_customer = {**full_customer, "customer_id": str(customer["id"])}
                 if not db["customer"].find_one(full_customer):
                     db["customer"].insert_one(full_customer)
                 
                 # get costumers image
                 customer_image = make_request(base_url + "/api/customers/" + str(customer["id"]) + "/image", True)
-                query = {"customer_id": str(customer["id"]), "image": customer_image}
+                query = {"customer_id": str(customer["id"]), "customer_image_id": str(customer["id"]),"image": customer_image}
                 if not db["customer_image"].find_one(query):
                     db["customer_image"].insert_one(query)
 
@@ -178,7 +196,6 @@ def fetch_customers():
                     db["customer"].update_one({"id": customer["id"]}, {"$set": {"encounters": encounter}})
                 
                 fetch_clothes(customer)
-                clear_progress_bar()
             except Exception as e:
                 treat_errors(e, base_url + "/api/customers/" + str(customer["id"]))
                 continue
@@ -186,7 +203,7 @@ def fetch_customers():
     except Exception as e:
         treat_errors(e, base_url + "/api/customers")
         pass
-    clear_screen()
+    
 
 
 def fetch_clothes(customer=None):
@@ -200,18 +217,17 @@ def fetch_clothes(customer=None):
         # --------------------- CLOTH ---------------------
         for cloth in clothes:
             try:
-                progress_bar(str(clothes.index(cloth)), str(len(clothes)), "cloth")
+                update_progress_bar(str(clothes.index(cloth)), str(len(clothes)), "cloth")
                 
                 #cloth image
                 cloth_image = make_request(base_url + "/api/clothes/" + str(cloth["id"]) + "/image", True)
                 if not cloth_image:
                     print("no image found for cloth id: " + str(cloth["id"]))
                 else:
-                    query = {"cloth_id": str(cloth["id"]), "image": cloth_image}
+                    query = {"cloth_id": str(cloth["id"]), "cloth_image_id": str(cloth["id"]), "customer_id": str(customer["id"]), "image": cloth_image}
                     if not db["cloth_image"].find_one({"cloth_id": str(cloth["id"])}):
                         db["cloth_image"].insert_one(query)
 
-                clear_progress_bar()
             except Exception as e:
                 treat_errors(e, base_url + "/api/clothes/" + str(cloth["id"]))
                 continue
@@ -228,21 +244,22 @@ def fetch_encounters():
         print("fetching encounters")
         for encounter in api_encounter:
             try:
-                progress_bar(str(api_encounter.index(encounter)), str(len(api_encounter)), "encounter")
-                if not db["smallencounter"].find_one(encounter):
-                    db["smallencounter"].insert_one(encounter)
+                update_progress_bar(str(api_encounter.index(encounter)), str(len(api_encounter)), "encounter")
+                encounter = {**encounter, "small_encounter_id": str(encounter["id"]), "encounter_id": str(encounter["id"])}
+                if not db["small_encounter"].find_one(encounter):
+                    db["small_encounter"].insert_one(encounter)
                     
                 full_encounter = make_request(base_url + "/api/encounters/" + str(encounter["id"]))
+                full_encounter = {**full_encounter, "encounter_id": str(encounter["id"])}
                 if not db["encounter"].find_one(full_encounter):
                     db["encounter"].insert_one(full_encounter)
-                clear_progress_bar()
             except Exception as e:
                 treat_errors(e, base_url + "/api/encounters/" + str(encounter["id"]))
                 continue
     except Exception as e:
         treat_errors(e, base_url + "/api/encounters")
         pass
-    clear_screen()
+    
         
 def fetch_tips():
     try:
@@ -250,18 +267,18 @@ def fetch_tips():
         api_tip = make_request(base_url + "/api/tips")
         for tip in api_tip:
             try:
-                progress_bar(str(api_tip.index(tip)), str(len(api_tip)), "tip")
-    
+                update_progress_bar(str(api_tip.index(tip)), str(len(api_tip)), "tip")
+
+                tip = {**tip, "tip_id": str(tip["id"])}
                 if not db["tip"].find_one(tip):
                     db["tip"].insert_one(tip)
             except Exception as e:
                 treat_errors(e, base_url + "/api/tips/" + str(tip["id"]))
                 continue
-            clear_progress_bar()
     except Exception as e:
         treat_errors(e, base_url + "/api/tips")
         pass
-    clear_screen()
+    
 
 def fetch_events():
     try:
@@ -269,39 +286,50 @@ def fetch_events():
         api_event = make_request(base_url + "/api/events")
         for event in api_event:
             try:
-                progress_bar(str(api_event.index(event)), str(len(api_event)), "event")
-                print("treating event id: " + str(event["id"]) + " progress: " + str(api_event.index(event)) + "/" + str(len(api_event)))
-                if not db["smallevent"].find_one(event):
-                    db["smallevent"].insert_one(event)
+                update_progress_bar(str(api_event.index(event)), str(len(api_event)), "event")
+                
+                event = {**event, "small_event_id": str(event["id"]), "event_id": str(event["id"])}
+                if not db["small_event"].find_one(event):
+                    db["small_event"].insert_one(event)
                 
                 full_event = make_request(base_url + "/api/events/" + str(event["id"]))
+                full_event = {**full_event, "event_id": str(event["id"])}
                 if not db["event"].find_one(full_event):
                     db["event"].insert_one(full_event)
-                clear_progress_bar()
             except Exception as e:
                 treat_errors(e, base_url + "/api/events/" + str(event["id"]))
                 continue
     except Exception as e:
         treat_errors(e, base_url + "/api/events")
         pass
-    clear_screen()
+    
         
 def fetch_all():
-    fetch_employee()
-    fetch_customers()
-    fetch_encounters()
-    fetch_tips()
-    fetch_events()
+    threads = [
+        threading.Thread(target=fetch_employee),
+        threading.Thread(target=fetch_customers),
+        threading.Thread(target=fetch_encounters),
+        threading.Thread(target=fetch_tips),
+        threading.Thread(target=fetch_events),
+    ]
+    # Start each thread
+    for thread in threads:
+        thread.start()
+
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
     clear_screen()
 
 try :
-    fetch_encounters()
-    fetch_tips()
-    fetch_events()
-    clear_screen()
+    fetch_all()
+    print_errors_summary()
+    print("Script finished")
 except Exception as e:
     treat_errors(e, "fetch_all")
     pass
 except KeyboardInterrupt as e:
+    clear_screen()
     print("Script Stopped")
     print_errors_summary()
