ff6a6ec9a392c59adc5c4c6698874b3820181b07
aimericdrk <aimeric.rouyer@epitech.eu>
2024-09-09 18:46:16 +0200

    :sparkles: adding the route to the newly added method fucntion to get our own account

--- server/src/router/api/api.query.js
+++ server/src/router/api/api.query.js
@@ -6,5 +6,6 @@ module.exports = function (app) {
     app.get("/soul_connection_api/:COLLECTIONNAME/:ID", get_user_from_session, api_interactions.soul_connection_api);
     app.get("/api/users", get_user_from_session, api_interactions.internal_api_get_all);
     app.get("/api/users/:ID", get_user_from_session, api_interactions.internal_api_get_one);
+    app.get("/api/me", get_user_from_session, api_interactions.internal_api_get_me);
 
 };
