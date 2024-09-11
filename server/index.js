6a88d0fcedc32c82524d900b7c03bf1bd3c55d03
aimericdrk <aimeric.rouyer@epitech.eu>
2024-09-11 23:38:28 +0200

    :lock: add a max file size upload limit

--- server/index.js
+++ server/index.js
@@ -3,13 +3,12 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 require("./src/database/mongo");
-//require("./scrapper/scrapper");
 
 const PORT = process.env.PORT || 3333;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({limit: '5mb', extended: true}));
 
 app.use((req, res, next) => {
     console.log(`Request: ${req.method} ${req.originalUrl}`);
